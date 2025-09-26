2bcbf23ed9e088368998569ef8005960d57f74e3
MinkoAlexandr <minkoalexandr@mail.ru>
2025-09-27 00:59:39 +0300

    task 2-5

--- Myshell.cpp
+++ Myshell.cpp
@@ -1,10 +1,33 @@
 #include <iostream>
 #include <string>
+#include <fstream>
 
 int main()
 {
-    std::cout<<"'";
+    std::string history="kubsh_history.txt";
+    std::ofstream history_file(history, std::ios::app);
+
     std::string input;
-    std::getline(std::cin,input);
-    std::cout<<input<<"\n";
+    while(std::getline(std::cin,input))
+    {
+        history_file<<input<<"\n";
+        history_file.flush();
+
+        if (input=="\\q")
+            break;
+    
+        if (input.empty()) continue;
+        if (input.find("echo")==0)
+            std::cout<<input.substr(6, input.length()-7)<<"\n";
+        else
+            std::cout<<input<<"\n";
+
+    }
+    
+    
+    //task1
+    //std::cout<<"'";
+    //std::string input;
+    //std::getline(std::cin,input);
+    //std::cout<<input<<"\n";
 }
